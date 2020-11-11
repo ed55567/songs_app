9a51e583a02fbd240f43cafd9df41cc88b4ca943
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-11-10 20:23:15 -0500

    added show index

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,5 +1,18 @@
 class Api::SongsController < ApplicationController
-  def index 
+  def index
     @songs = Song.all
     render "index.json.jb"
+  end
+
+  def create
+    @songs = song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+
+    @song.save
+    render "show.json.jb"
+  end
 end
